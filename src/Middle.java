b8ae4fad4a7157f91826829d57c7a5bf82620740
HMonique <161358207+HMonique@users.noreply.github.com>
2024-07-23 15:33:29 -0400

    Commit 1

---
+++ src/Middle.java
@@ -0,0 +1,7 @@
+public class Middle {
+    public static void main(String[] args) {
+        int [] middle = {13, 5, 7, 68, 2};
+        for (int i = 0; i < middle.length; i++);
+        System.out.println(middle[2]);
+    }
+}
